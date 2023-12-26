d4f4f5a0de9dbb85d453257ab8a6aea8e8193378
Guillermo Santos <santosguillermo360@outlook.com>
2023-12-26 19:36:11 -0400

    Update SessionManagerService.cs
    Added static instance, changed message send to the debugger and added missing overrides.

--- src/Zaphyros.Core/Users/Services/SessionManagerService.cs
+++ src/Zaphyros.Core/Users/Services/SessionManagerService.cs
@@ -5,9 +5,20 @@ namespace Zaphyros.Core.Users.Services
     internal class SessionManagerService : Service
     {
         private List<Session> _sessions;
+        private static SessionManagerService _instance;
+
+        public static SessionManagerService Instance => _instance ??= new();
         public SessionManagerService() : base()
         {
             _sessions = new();
+        }
+
+        public override void BeforeStart()
+        {
+            foreach (var session in _sessions)
+            {
+                session.Start();
+            }
         }
 
         public override void Update()
@@ -18,9 +29,17 @@ namespace Zaphyros.Core.Users.Services
             }
         }
 
+        public override void AfterStart()
+        {
+            foreach (var session in _sessions)
+            {
+                session.Stop();
+            }
+        }
+
         internal void RegisterSession(Session session)
         {
-            Sys.Global.Debugger.Send($"Registering Session {session.GetType().Name}");
+            Sys.Global.Debugger.Send($"Registering {session.GetType().Name}-{session.Name}");
             _sessions.Add(session);
         }
     }
